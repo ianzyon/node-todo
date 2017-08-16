b0727b98dc73ccd5c0a294014a1db8fc03ad2f30
Ian Vieira <iansalgado@yahoo.com.br>
2017-08-15 21:42:12 -0300

    mlab set as addon on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // usa o Promise do ES6
-mongoose.connect('mongodb://localhost:27017/TodoApp', 
+// mlab mongodb_uri: mongodb://heroku_8b961v2h:tabsjsbdabqe133biaceb6p806@ds131742.mlab.com:31742/heroku_8b961v2h
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', 
 {
     useMongoClient: true
 });
