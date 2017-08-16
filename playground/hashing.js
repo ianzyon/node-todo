ef687ffd95f9aa98de45ab46eb10049e11023fe5
Ian Vieira <iansalgado@yahoo.com.br>
2017-08-16 20:22:33 -0300

    hashing password before post

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,32 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt)=> { // tempo de rounds , callback contendo o salt
+    bcrypt.hash(password, salt, (err, hash)=> { // hash function recebe a senha, o salt e um call back que recebe o hash pronto
+        console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('Decode', decoded);
+var hPassword = '$2a$10$p92VdzPxiw2oDBRCLqO4zO7Y0oPI3/WehvjNiQzApieYlAGdtXyjy';
+
+bcrypt.compare(password, hPassword, (err,res)=>{
+    console.log(res);
+});
+
+
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('Decode', decoded);
 
 // var message = 'I am user number 3';
 
